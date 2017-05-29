50966b9fbf46d7ece025c41a5a2ede5d88e2ff30
sarahxjohari <saraholewin@gmail.com>
2017-05-29 16:21:36 -0400

    i made things do stuff.

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,11 +47,11 @@
 
 		// Off-Canvas Navigation.
 
-			// Title Bar.
+			// Title Bar. Added the a href language so it's a link. -SL
 				$(
 					'<div id="titleBar">' +
 						'<a href="#navPanel" class="toggle"></a>' +
-						'<span class="title">' + $('#logo').html() + '</span>' +
+						'<span class="title"> <a href="index.html">' + $('#logo').html() + '</a></span>' +
 					'</div>'
 				)
 					.appendTo($body);
